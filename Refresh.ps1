8c31ed0df13d4af9119ab0ba24e053847d75af17
Tuckn <tuckn333@gmail.com>
2023-02-24 16:39:51 +0900

    Add wait

--- Refresh.ps1
+++ Refresh.ps1
@@ -6,7 +6,7 @@ Param(
     [String] $BookPath,
 
     [Parameter(Position = 1)]
-    [Switch] $ShowsWindow
+    [Switch] $HiddenWindow
 )
 
 $ErrorActionPreference = "Stop"
@@ -16,18 +16,24 @@ Set-StrictMode -Version 2.0
 Get-Item -Path $BookPath | Set-ItemProperty -Name IsReadOnly -Value $false
 
 $excel = New-Object -ComObject Excel.Application
-$excel.Visible = !($ShowsWindow)
+$excel.Visible = !($HiddenWindow)
 # $excel.DisplayAlerts = $false
 
 $book = $excel.Workbooks.Open($BookPath, $null, $false, $true)
+
 $book.refreshall()
+Start-Sleep -Seconds 3
 # $book.Unprotect() # Not working
+
 $book.Save()
+Start-Sleep -Seconds 3
+
 # $book.Protect()
 $book.Close()
+Start-Sleep -Seconds 3
 
 $excel.Quit()
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel)
 
 # Setting IsReadOnly property to $True
-Get-Item -Path $BookPath | Set-ItemProperty -Name IsReadOnly -Value $true
+# Get-Item -Path $BookPath | Set-ItemProperty -Name IsReadOnly -Value $true
